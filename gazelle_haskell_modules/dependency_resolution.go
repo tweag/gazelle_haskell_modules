93464a76ec6b088c00c003d4b0185db0a3a6e860
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-11-22 19:47:39 -0300

    Do not add cross-library dependencies in haskell_module rules.

--- gazelle_haskell_modules/dependency_resolution.go
+++ gazelle_haskell_modules/dependency_resolution.go
@@ -147,15 +147,9 @@ func setHaskellModuleDepsAttribute(
 // as the given moduleName, the rule defining the module for the given component is
 // chosen.
 //
-// 3. If only one library in mapDep contains the given module,
-// it is chosen.
+// 3. If multiple rules define the module, an error is returned.
 //
-// 4. If there is only one rule defining the module in the repo,
-// it is chosen.
-//
-// 5. If multiple rules define the module, an error is returned.
-//
-// 6. If no rule defines the module, nil is returned.
+// 4. If no rule defines the module, nil is returned.
 //
 func findModuleLabelByModuleName(
 	ix *resolve.RuleIndex,
@@ -197,24 +191,6 @@ func findModuleLabelByModuleName(
 				finalLabel = &lbl
 			} else {
 				return nil, fmt.Errorf("Multiple rules define %s in %v: %v %v", moduleName, originLabel, *finalLabel, r.Label)
-			}
-		}
-	}
-	if finalLabel != nil {
-		return finalLabel, nil
-	}
-
-	for _, r := range res {
-		// Here we assume <library>.<module_name> scheme for haskell_module rule names
-		// See Note [haskell_module naming scheme].
-		pkgName := strings.SplitN(r.Label.Name, ".", 2)[0]
-		pkgLabel := label.New(r.Label.Repo, r.Label.Pkg, pkgName)
-		if _, ok := mapDep[pkgLabel]; ok {
-			if finalLabel == nil {
-				lbl := rel(r.Label, from)
-				finalLabel = &lbl
-			} else {
-				return nil, fmt.Errorf("Multiple rules define %s in library dependencies: %v %v", moduleName, originalComponentName, *finalLabel, r.Label)
 			}
 		}
 	}
