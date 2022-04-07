0d6abc1e0eb10000dd22c8d74d5a7fc0d4622e54
Georgi Lyubenov <godzbanebane@gmail.com>
2022-04-07 10:50:42 +0300

    Use example to test adding a new module

--- example/package-c/add-module-src/PackageC/Other/B.hs
+++ example/package-c/add-module-src/PackageC/Other/B.hs
@@ -1,3 +1,4 @@
 module PackageC.Other.B where
 
 import Data.Void.Unsafe ()
+import PackageC.Exposed.NewModule()
--- example/package-c/another-src/PackageC/Exposed/D.hs
+++
@@ -1,6 +0,0 @@
-module PackageC.Exposed.D where
-
-import PackageC.Other.B()
-
-f :: forall a. a -> a
-f x = x
